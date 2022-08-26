72924b9ebdd78f8834f5415bce5500e972070f0d
Lucas Dainez <lucdainez@gmail.com>
2022-08-26 19:30:39 -0300

    feat(Componente de classe Header): arrumar cabeçalho
    Aqui foi puxado a chave email do estado do Redux e renderizado no componente além de criar outros conteúdos no componente

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
   render() {
+    const { email } = this.props;
     return (
-      <div>Header</div>
+      <header>
+        <p data-testid="email-field">{email}</p>
+        <p data-testid="total-field">0</p>
+        <p data-testid="header-currency-field">BRL</p>
+      </header>
     );
   }
 }
 
-export default Header;
+Header.propTypes = {
+  email: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({ email: state.user.email });
+
+export default connect(mapStateToProps)(Header);
