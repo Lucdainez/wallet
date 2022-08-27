4ef44aed1cdabab865f8976684ed52d1c23bf674
Lucas Dainez <lucdainez@gmail.com>
2022-08-27 17:14:20 -0300

    feat(Componente de classe Wallet): renderizar componente
    o componente de classe WalletForm está sendo renderizado no Wallet

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Header from '../components/Header';
+import WalletForm from '../components/WalletForm';
 
 class Wallet extends React.Component {
   render() {
     return (
-      <Header />
+      <>
+        <header>
+          <Header />
+        </header>
+        <main>
+          <WalletForm />
+        </main>
+      </>
     );
   }
 }
