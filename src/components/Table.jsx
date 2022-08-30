1b1c1313315788846023c4c167e07bc09957b3ed
Lucas Dainez <lucdainez@gmail.com>
2022-08-30 17:02:40 -0300

    feat(Componente de classe Table): iniciar o componente
    foi criado uma tabela com alguns valores

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <table>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* {expenseToTable} */}
+        </tbody>
+      </table>
     );
   }
 }
 
-export default Table;
+// const mapStateToProps = (state) => ({
+
+// });
+
+export default connect()(Table);
--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import WalletForm from '../components/WalletForm';
+import Table from '../components/Table';
 
 class Wallet extends React.Component {
   render() {
@@ -11,6 +12,7 @@ class Wallet extends React.Component {
         </header>
         <main>
           <WalletForm />
+          <Table />
         </main>
       </>
     );
