54acbe6397e35411485ba4c2cd637eafd63c6d58
Lucas Dainez <lucdainez@gmail.com>
2022-08-30 12:17:14 -0300

    feat(Componente de classe Header): renderizar valor total
    aqui recebemos o valor com a cotação e fazemos um reduce para somar os valores anteriores ao novo e renderizar na tela

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import { string } from 'prop-types';
 
 class Header extends Component {
   render() {
-    const { email } = this.props;
+    const { email, value } = this.props;
+    const SUM_VALUE = value.reduce((acc, curr) => acc + Number(curr), 0);
     return (
       <section>
         <p data-testid="email-field">{email}</p>
-        <p data-testid="total-field">0</p>
+        <p
+          data-testid="total-field"
+        >
+          { SUM_VALUE }
+        </p>
         <p data-testid="header-currency-field">BRL</p>
       </section>
     );
@@ -16,9 +21,12 @@ class Header extends Component {
 }
 
 Header.propTypes = {
-  email: PropTypes.string.isRequired,
-};
+  email: string,
+}.isRequired;
 
-const mapStateToProps = (state) => ({ email: state.user.email });
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+  value: state.wallet.expensesValue,
+});
 
 export default connect(mapStateToProps)(Header);
